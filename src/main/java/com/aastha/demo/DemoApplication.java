6d2a5370596263bab5401baaeaaac6e13f6620bd
aasthaghosh <aasthaghosh15@gmail.com>
2026-07-13 11:30:34 +0530

    Spring Boot Demo1

---
+++ src/main/java/com/aastha/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.aastha.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		Student student = new Student(21, "Aastha");
+
+		System.out.println(student.getAge());
+		System.out.println(student.getName());
+	}
+}
